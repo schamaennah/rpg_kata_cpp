cde73bceeca4db3c95be77b44bbc293568132253
schamaennah <david.schamaennah@vk.com>
2024-09-24 17:04:03 +0200

    When reduced to 0 Health, Magical Objects are Destroyed

--- tests/magical_object_tests.cpp
+++ tests/magical_object_tests.cpp
@@ -9,4 +9,10 @@ TEST_CASE("Magical Objects can be Created", "[magical object]")
     constexpr auto new_magical_object = magical_object{magical_object_health{100U}};
 }
 
+TEST_CASE("Magical Objects with zero health are Destroyed", "[magical object]")
+{
+    REQUIRE(magical_object_health{0}.is_destroyed() == true);
+    REQUIRE(magical_object_health{10}.is_destroyed() == false);
+}
+
 } // namespace rpg_kata::tests
