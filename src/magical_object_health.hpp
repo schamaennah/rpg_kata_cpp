adb30a3c2b405c1d2fb19b97ff46bb30dce3871c
schamaennah <david.schamaennah@vk.com>
2024-09-24 16:59:11 +0200

    The maximum amount of Health is fixed at the time the object is created

--- src/magical_object_health.hpp
+++ src/magical_object_health.hpp
@@ -5,6 +5,21 @@
 
 namespace rpg_kata
 {
-using magical_object_health = value_wrapper<new_type, unsigned, 1U>;
+class magical_object_health
+{
+private:
+    using primitive_type = unsigned;
+    using type           = value_wrapper<new_type>;
+
+public:
+    explicit constexpr magical_object_health(const primitive_type max_health)
+        : current_health{max_health}
+        , max_health{max_health}
+    {}
+
+private:
+    type current_health;
+    type max_health;
+};
 
 } // namespace rpg_kata
