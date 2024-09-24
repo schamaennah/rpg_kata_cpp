0d7cefb9681d274a6f794b44ce0051fe6c198553
schamaennah <david.schamaennah@vk.com>
2024-09-24 14:30:01 +0200

    When reduced to 0 Health, things are Destroyed

--- src/thing_health.hpp
+++ src/thing_health.hpp
@@ -7,4 +7,9 @@ namespace rpg_kata
 {
 using thing_health = value_wrapper<new_type, double, 1.>;
 
+constexpr bool is_destroyed(const thing_health& self)
+{
+    return self == thing_health{0};
+}
+
 } // namespace rpg_kata
--- tests/thing_tests.cpp
+++ tests/thing_tests.cpp
@@ -8,4 +8,10 @@ TEST_CASE("Things can be Created", "[thing]")
 {
     const auto new_thing = thing{};
 }
+
+TEST_CASE("Things with zero health are Destroyed", "[thing]")
+{
+    REQUIRE(is_destroyed(thing_health{0}) == true);
+    REQUIRE(is_destroyed(thing_health{10}) == false);
+}
 } // namespace rpg_kata::tests
