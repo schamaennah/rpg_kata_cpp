14118b765ed2a6d67dc5e702710ca4847adea495
schamaennah <david.schamaennah@vk.com>
2024-09-24 14:22:52 +0200

    Characters can damage Things which are in Range

--- src/thing_health.hpp
+++ src/thing_health.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
+#include "damage.hpp"
 #include "new_type.hpp"
+#include "non_negative_double.hpp"
 #include "value_wrapper.hpp"
 
 namespace rpg_kata
 {
-using thing_health = value_wrapper<new_type, unsigned, 1>;
+using thing_health = value_wrapper<new_type, non_negative_double, 1.>;
+
+constexpr thing_health& operator-=(thing_health& self, const damage& damage)
+{
+    self.value -= damage.value;
+    return self;
+}
 
 } // namespace rpg_kata
